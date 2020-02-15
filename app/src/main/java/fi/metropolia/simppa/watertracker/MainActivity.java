082d0e1460e6fd56cdd587f7c4f4a065a605d6c8
qufeihua <qufeihua@gmail.com>
2020-02-16 01:57:47 +0200

    Spinner in the mainactivity works

--- app/src/main/java/fi/metropolia/simppa/watertracker/MainActivity.java
+++ app/src/main/java/fi/metropolia/simppa/watertracker/MainActivity.java
@@ -1,16 +1,28 @@
 package fi.metropolia.simppa.watertracker;
 
+import fi.metropolia.simppa.watertracker.database.Unit;
+import fi.metropolia.simppa.watertracker.database.UnitViewModel;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Button b1, b2, b3;
     Intent intent;
+    private ArrayList<String> unitNameList= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,42 @@ public class MainActivity extends AppCompatActivity {
         b1 = findViewById(R.id.button);
         b2 = findViewById(R.id.button2);
         b3 = findViewById(R.id.setDailyGoalButton);
+
+
+        /*
+         * From Feihua
+         * populate the spinner in the main_activity
+         *
+          */
+        //get view Model
+        UnitViewModel unitViewModel= new ViewModelProvider(this).get(UnitViewModel.class);
+        //get all units from database through view model and live data
+        unitViewModel.getUnitList().observe(this,new Observer<List<Unit>>(){
+
+            @Override
+            public void onChanged(List<Unit> units) {
+                Log.d("my",""+units.size());
+                //empty the list so every item are not populate again and again
+                unitNameList.clear();
+                for(Unit unit:units){
+                    unitNameList.add(unit.getUnitName());
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, unitNameList);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                Spinner spinner=findViewById(R.id.main_spinner_chooseUnit);
+                spinner.setAdapter(adapter);
+
+
+
+            }
+        });//end of unitViewModel.getUnitList().observe
+
+
+
+
+
+
+
     }
 
     public void onButton(View view) {
@@ -34,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
 
 
 }
