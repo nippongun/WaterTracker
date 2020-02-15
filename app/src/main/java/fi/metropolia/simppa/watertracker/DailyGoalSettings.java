36b92b00957b6696ecfd2a2c587dcd3230f59ae8
KlenJavor <55927646+KlenJavor@users.noreply.github.com>
2020-02-15 20:52:23 +0200

    Removed top bar from activity_goal.xml

--- app/src/main/java/fi/metropolia/simppa/watertracker/DailyGoalSettings.java
+++ app/src/main/java/fi/metropolia/simppa/watertracker/DailyGoalSettings.java
@@ -4,13 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DailyGoalSettings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_goal);
         Intent intent = getIntent();
+
+        hideNavigationBar();
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        hideNavigationBar();
+    }
+
+    private void hideNavigationBar() {
+        this.getWindow().getDecorView()
+                .setSystemUiVisibility(
+        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
 }
