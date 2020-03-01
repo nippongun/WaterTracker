23f5fee29d1506417e212ef67d3a552549b1fb18
qufeihua <qufeihua@gmail.com>
2020-03-01 18:35:42 +0200

    Merge branch 'Feihua' of https://github.com/nippongun/WaterTracker into Feihua
    Now chart should work and frontpage pie chart should work also
    
    corrected the date should go back 5 days not forward 5 days
    
    change the date format in the chart

--- app/src/main/java/fi/metropolia/simppa/watertracker/MainActivity.java
+++ app/src/main/java/fi/metropolia/simppa/watertracker/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
         //get today's year month and day then set Date from as the bigining of the day, Date to as the end of the day.
         int year = Calendar.getInstance().get(Calendar.YEAR);
-        int month = Calendar.getInstance().get(Calendar.MONTH);
+        int month = Calendar.getInstance().get(Calendar.MONTH);//month give you a value start from 0
         int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
         Calendar cal = Calendar.getInstance();
         cal.setTimeInMillis(0);
@@ -157,7 +157,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });//end of the spinner listener
     }
-
+    /**update the piechart again onResume
+     * **/
     @Override
     public void onResume() {
         super.onResume();
