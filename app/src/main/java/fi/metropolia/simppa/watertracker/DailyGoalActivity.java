1f994b466e449022a9179205a520a46209439de7
KlenJavor <55927646+KlenJavor@users.noreply.github.com>
2020-03-03 23:30:36 +0200

    Input validation included

--- app/src/main/java/fi/metropolia/simppa/watertracker/DailyGoalActivity.java
+++ app/src/main/java/fi/metropolia/simppa/watertracker/DailyGoalActivity.java
@@ -1,7 +1,6 @@
 package fi.metropolia.simppa.watertracker;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
